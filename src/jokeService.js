821ca8bf8df8f2b12e32977dcf52cfacccf01f81
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2023-01-30 14:33:19 -0800

    removed excess comments

--- src/jokeService.js
+++ src/jokeService.js
@@ -4,12 +4,10 @@ let jokeService = () => {
   const config = {
     method: "GET",
     url: "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=5",
-    //crossdomain: true,
     headers: { "Content-Type": "application/json" },
   };
 
   return axios(config);
 };
 
-//export { getJoke };
 export default jokeService;
