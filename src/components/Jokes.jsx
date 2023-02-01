1dd45efb74b1eb85bd55fcfa81a4beec2dfd05cd
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2023-01-31 21:57:16 -0800

    playing around with font

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -47,6 +47,7 @@ function Jokes() {
     return (
       <div
         className="col-4"
+        id={aJoke.id}
         key={"ListA-" + aJoke.id}
         style={{
           display: "flex",
@@ -84,6 +85,18 @@ function Jokes() {
       <img src={headerpresentation} alt="loading..." />
 
       <div className="row">{showJokes && jokeData}</div>
+
+      <div className="card">
+        <h5 className="card-header">Featured</h5>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          {showJokes && jokeData}
+        </div>
+      </div>
 
       <button type="button" id="showHideCharBtn" onClick={onToggleJokes}>
         {" "}
